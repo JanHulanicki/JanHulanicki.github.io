55ebf10b1ce4ddfa325a6ef2d54183b08d11763a
Jan Hulanicki <jan_hulanicki@poczta.onet.pl>
2020-01-30 21:04:28 +0100

    Initial commit2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'http://sheltered-sands-84865.herokuapp.com/v1/task/';
+  var apiRoot = 'https://sheltered-sands-84865.herokuapp.com/v1/task/';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
